25ea75e647bac2bf31180239772ceb97f47488da
Aaron Miller <acmiller273@gmail.com>
2016-05-15 20:50:51 -0400

    Simplified solution to 2016-1A-B

--- 2016/1C/B.py
+++ 2016/1C/B.py
@@ -8,23 +8,14 @@ def solve():
     B, M = map(int, raw_input().split())
 
     # solve
-    if M > 2**(B-2):
+    if M > 2**(B - 2):
         return 'IMPOSSIBLE'
     else:
-        out = 'POSSIBLE\n'
         bstring = '{0:b}'.format(M-1).rjust(B-2, '0')
-        out += '0'
-        for i in xrange(B-2):
-            if bstring[i] == '1':
-                out += '1'
-            else:
-                out += '0'
-        out += '1\n'
-
-        for i in xrange(1, B):
-            out += ''.join('0' if j <= i else '1' for j in xrange(B))
-            out += '\n'
-        return out[:-1]
+        out = 'POSSIBLE\n'
+        out += '0%s1\n'%(bstring[:B - 2])
+        out += '\n'.join('0'*(i + 1) + '1'*(B - i - 1) for i in xrange(1, B))
+        return out
 
 T = int(raw_input())
 for case in xrange(T):
