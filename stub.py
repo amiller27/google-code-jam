ddf515d713ab935f36ea3e267957f55b035a8cd7
Aaron Miller <acmiller273@gmail.com>
2017-04-08 20:08:00 -0400

    Changed tabs to spaces in stub.py

--- stub.py
+++ stub.py
@@ -3,13 +3,13 @@ import sys
 import itertools
 
 def solve():
-	# parse input
+    # parse input
 
 
-	# solve
+    # solve
 
 
 T = int(raw_input())
 for case in xrange(T):
-	print(case, file=sys.stderr)
-	print("Case #%d: %s"%(case+1, solve()))
+    print(case, file=sys.stderr)
+    print("Case #%d: %s"%(case+1, solve()))
